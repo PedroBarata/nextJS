ee1896e48ab675754529dcb24e8eb62208ffdd40
Pedro Barata <pedro.barata@fasolti.com.br>
2019-09-27 08:43:45 -0300

    initial commit

---
+++ pages/auth/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const indexAuthPage = () => {
+    return (
+        <div>
+            <h1>The auth Page!</h1>
+        </div>
+    )
+}
+
+export default indexAuthPage;
