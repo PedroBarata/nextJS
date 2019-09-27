1c46d8fe69ad58d29bd0729c7737c2bf0b16e1d4
Pedro Barata <pedro.barata@fasolti.com.br>
2019-09-27 09:19:07 -0300

    feat(425): A Special lifecycle Hook

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
 
-const indexAuthPage = () => {
-    return (
-        <div>
-            <h1>The auth Page!</h1>
-        </div>
-    )
-}
+const indexAuthPage = (props) => {
+  return (
+    <div>
+      <h1>The auth Page - {props.appName}</h1>
+    </div>
+  );
+};
+
+indexAuthPage.getInitialProps = context => {
+  console.log(context);
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({ appName: "Super App (Auth)" });
+    }, 1000);
+  });
+  return promise;
+};
 
 export default indexAuthPage;
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,44 @@
-import React from 'react';
-import Link from 'next/link';
-import Router from 'next/router';
+import React, { Component } from "react";
+import Link from "next/link";
+import Router from "next/router";
 
-const indexPage = () => {
-    return (
+class IndexPage extends Component {
+    static getInitialProps(context) {
+        console.log(context);
+        const promise = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve({appName: 'Super App'});
+            }, 1000);
+        })
+        return promise;
+    }
+    render() {
+        return (
         <div>
-            <h1>The main Page!</h1>
-            <p>Go to <Link href='/auth'><a>Auth</a></Link></p>
-            <button onClick={() => Router.push('/auth')}>Go to auth</button>
-            <Link  href='/auth'><a>GO TO AUTH</a></Link>
+            <h1>The main page of {this.props.appName}!</h1>
+            <p>
+            Go to{" "}
+            <Link href="/auth">
+                <a>Auth</a>
+            </Link>
+            </p>
+            <button onClick={() => Router.push("/auth")}>Go to auth</button>
+            <Link href="/auth">
+            <a>GO TO AUTH</a>
+            </Link>
 
             <style jsx>
             {`
                 div {
-                    border: 1px solid #eee;
-                    box-shadow: 1px solid #ccc;
-                    padding: 20px;
-                    text-align: center;
+                border: 1px solid #eee;
+                box-shadow: 1px solid #ccc;
+                padding: 20px;
+                text-align: center;
                 }
             `}
             </style>
         </div>
-    )
+        );
+    }
 }
-
-export default indexPage;
+export default IndexPage;
