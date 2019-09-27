1b083dcc0f102844c9efa8818d2b56130f2708e6
Pedro Barata <pedro.barata@fasolti.com.br>
2019-09-27 08:59:11 -0300

    feat(424): Styling NextJS

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,17 @@ const indexPage = () => {
             <p>Go to <Link href='/auth'><a>Auth</a></Link></p>
             <button onClick={() => Router.push('/auth')}>Go to auth</button>
             <Link  href='/auth'><a>GO TO AUTH</a></Link>
+
+            <style jsx>
+            {`
+                div {
+                    border: 1px solid #eee;
+                    box-shadow: 1px solid #ccc;
+                    padding: 20px;
+                    text-align: center;
+                }
+            `}
+            </style>
         </div>
     )
 }
